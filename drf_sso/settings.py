1393b1544cf6d37414656a46ab0282db917deb03
EL OUAZIZI Walid <Walid.El.Ouazizi@genavir.fr>
2025-05-22 15:32:48 +0200

    Implémentation plus correcte de la maj des utilisateurs

--- drf_sso/settings.py
+++ drf_sso/settings.py
@@ -4,16 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 DEFAULTS = {
     "MODULE_BASE_URL": None,
     "FRONTEND_CALLBACK_URL": None,
-    "PROVIDERS": {},
-    "BASE_USER_POPULATION": {
-        "LOOKUP_FIELD": ("email", "email"),
-        "MAPPINGS": {
-            "sn": "last_name",
-            "givenname": "first_name",
-            "email": "email",
-            "username": "username"
-        }
-    }
+    "PROVIDERS": {}
 }
 
 class DRFSSOSettings:
