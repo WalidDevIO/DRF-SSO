ba398a4398fd379e16d8b37fdc4e4700a1c7a4cf
EL OUAZIZI Walid <Walid.El.Ouazizi@genavir.fr>
2025-05-22 15:33:47 +0200

    Typo

--- drf_sso/providers/user_population.py
+++ drf_sso/providers/user_population.py
@@ -9,7 +9,7 @@ def base_user_population(payload, name):
         (payload_lookup, db_lookup) = populate_user_conf['lookup_field']
         mappings = populate_user_conf['mappings']
     except KeyError:
-        raise ImproperlyConfigured('Provider "{name}" is using default user population method but doesn\'t provide a valid configuration.')
+        raise ImproperlyConfigured(f'Provider "{name}" is using default user population method but doesn\'t provide a valid configuration.')
     
     user = User.objects.get_or_create(**{db_lookup: payload[payload_lookup]})
     for payload_field, db_field in mappings.items():
