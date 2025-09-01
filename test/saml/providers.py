ce8bd8ea0207738f0e2e2b5bd0770cf9f2afed24
EL OUAZIZI Walid <Walid.El.Ouazizi@genavir.fr>
2025-09-01 15:39:22 +0200

    Maj test SAML

--- test/saml/providers.py
+++ test/saml/providers.py
@@ -1,9 +1,12 @@
-from samlsp.config import SPConfig, IdPConfig
+from drf_sso.providers.samlsp.config import SPConfig, IdPConfig
+from drf_sso.providers.samlsp import SAMLSP as _SAMLSP
 from pathlib import Path
 
 BASE_DIR = Path("test", "saml")
 
 OUT_DIR = BASE_DIR / "out"
+
+OUT_DIR.mkdir(exist_ok=True, parents=True)
 
 CONF_DIR = BASE_DIR / "conf"
 
@@ -12,3 +15,4 @@ CERTS_DIR = CONF_DIR / "certs"
 SP = SPConfig.from_file(CONF_DIR / "sp_config.json")
 
 IDP = IdPConfig.from_url("https://idp-notilus.ifremer.fr/idp/metadata")
+SAMLSP = _SAMLSP(SP, IDP)
--- test/saml/req.py
+++ test/saml/req.py
@@ -1,15 +1,12 @@
 from .providers import SP, IDP, OUT_DIR
-from samlsp.request import create_authn_request
-from samlsp.config import Binding
-import os
+from drf_sso.providers.samlsp.request import create_authn_request
+from drf_sso.providers.samlsp.config import Binding
 
 (sso_endpoint, html_auto_submit_req, req) = create_authn_request(SP, IDP, Binding.HTTP_POST)
 print(sso_endpoint)
 
 TEST_OUT = OUT_DIR / "authnreq"
-
-if not TEST_OUT.exists():
-    os.makedirs(TEST_OUT)
+TEST_OUT.mkdir(exist_ok=True, parents=True)
 
 TEST_OUT.joinpath("post.html").write_text(html_auto_submit_req)
 TEST_OUT.joinpath("request.xml").write_text(req._build_xml())
